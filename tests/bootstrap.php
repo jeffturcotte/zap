c61722f94c454cf5f883da2d698ac81f0079a910
Kevin Hamer <kevin.hamer@gmail.com>
2014-02-04 15:32:00 -0500

    fixing PHPUnit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,3 +6,19 @@ if (!file_exists($autoloader)) {
 }
 
 include $autoloader;
+
+class ClosureResolver implements \Zap\ResolverInterface {
+	private $callback = null;
+
+	public function __construct($callback)
+	{
+		$this->callback = $callback;
+	}
+
+	public function resolve(\Zap\App $app)
+	{
+		return $app->invoke($this->callback);
+	}
+}
+
+class Example extends ArrayObject {}
